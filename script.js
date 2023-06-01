e826b421d336341588646c1cfc868718f722db79
Oleksandr Mykhailovskyi <oleksandrmykhailovskyi@macbook-pro-oleksandr.home>
2023-06-01 21:18:53 +0200

    Adding selector with options for input from and to. Adding max date

--- script.js
+++ script.js
@@ -1,149 +1,148 @@
-const back = document.querySelector('.back');
+const back = document.getElementById('back');
 const wayBackChange = document.getElementById('wayBack');
-const inputFormElement = document.getElementById('start-station');
-const selectStation = document.getElementById('selectStation');
+const inputStartStation = document.getElementById('start-station');
+const selectStartStation = document.getElementById('select-station');
+const inputArrivalStation = document.getElementById('arrival-station');
+const selectArivalStation = document.getElementById('select-arrival-station');
 
-inputFormElement.addEventListener('input', function () {
-	const inputValue = inputFormElement.value;
-	selectStation.value = inputValue;
-})
 
-selectStation.addEventListener('change', function () {
-	const selectedValue = selectStation.value;
-	inputFormElement.value = selectedValue;
-})
-
-// console.log("selectStation ", selectStation);
-
-// console.log('Input:', inputElement.value);
-
-// show back way
-wayBackChange.addEventListener('click', showBaclPanel);
-
-// listening input value
-// inputFromElement.addEventListener('keyup', function (event) {
-// 	const inputValue = event.target.value;
-//   });
-
-// select station
-// selectStation.addEventListener('change', function (event) {
-// 	console.log('Selected option:', event.target.value);
-// 	inputFromElement.value = event.target.value;
-//   });
+inputStartStation.addEventListener('click', changeValueInput);
+selectStartStation.addEventListener('change', selectListStation); 
+wayBackChange.addEventListener('click', showBackPanel);
+inputArrivalStation.addEventListener('click', changeValueArrivalInput);
+selectArivalStation.addEventListener('change', selectListArrivalStation);
 
 const stations = [
 	{
 		name: 'International Space Station (ISS)',
 		region: 'USA',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'Chinese Space Station (CSS)',
 		region: 'China',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'Mir Space Station',
 		region: 'Russia',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'Salyut Space Station',
 		region: 'Russia',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'Soyuz Space Station',
 		region: 'Russia',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'Tiangong Space Station',
 		region: 'China',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'Skylab',
 		region: 'USA',
-		planet: "Earth"
+		planet: 'Earth',
 	},
 	{
 		name: 'SpaceHab',
 		region: 'USA',
-		planet: "Earth"
+		planet: 'Earth',
+	},
+	{
+		name: 'Curiosity Rover',
+		region: 'Gale Crater',
+		planet: 'Mars',
+	},
+
+	{
+		name: 'Perseverance Rover',
+		region: 'Jezero Crater',
+		planet: 'Mars',
+	},
+
+	{
+		name: 'InSight Lander',
+		region: 'Elysium Planitia',
+		planet: 'Mars',
+	},
+
+	{
+		name: 'Spirit Rover',
+		region: 'Gusev Crater',
+		planet: 'Mars',
+	},
+
+	{
+		name: 'Opportunity Rover',
+		region: 'Meridiani Planum',
+		planet: 'Mars',
 	},
 ];
 
-const stationsMars =[
-	{
-		name: "Curiosity Rover",
-		region: "Gale Crater",
-		planet: "Mars"
-		},
-		
-		{
-		name: "Perseverance Rover",
-		region: "Jezero Crater",
-		planet: "Mars"
-		},
-		
-		{
-		name: "InSight Lander",
-		region: "Elysium Planitia",
-		planet: "Mars"
-		},
-		
-		{
-		name: "Spirit Rover",
-		region: "Gusev Crater",
-		planet: "Mars"
-		},
-		
-		{
-		name: "Opportunity Rover",
-		region: "Meridiani Planum",
-		planet: "Mars"
-		}
-]
+for (const el of stations) {
+	const option = document.createElement('option');
+	option.value = el.name;
+	option.textContent = `${el.name},${el.region},${el.planet}`;
 
+	selectStartStation.appendChild(option);
+}
+
+for (const el of stations) {
+	const option = document.createElement('option');
+	option.value = el.name;
+	option.textContent = `${el.name},${el.region},${el.planet}`;
+
+	selectArivalStation.appendChild(option);
+}
 
 // for date input
-const today = new Date().toISOString().split('T')[0];
-document.getElementById('date').setAttribute('min', today)
+const minLimitDate = new Date().toISOString().split('T')[0];
+document.getElementById('date').setAttribute('min', minLimitDate);
 
-function showBaclPanel(){
-	// console.log("change")
+// max Limit Date
+const currentDate = new Date();
+const maxDate = new Date();
+maxDate.setFullYear(currentDate.getFullYear() + 1);
+document.getElementById('date').setAttribute('max', maxDate.toISOString().split('T')[0]);
+
+
+function showBackPanel() {
 	back.style.display === 'flex'
 		? (back.style.display = 'none')
 		: (back.style.display = 'flex');
 };
 
-// function selecStation(str) {
-	
-// }
+// choosing of start station
+function selectListStation() {
+	const selectedValue = selectStartStation.value;
+	console.log("selectedValue", selectedValue);
+	inputStartStation.value = selectedValue;
+	//  close list
+	selectStartStation.setAttribute('hidden', 'true');
+};
 
+function changeValueInput() {
+	const inputValue = inputStartStation.value;
+	selectStartStation.value = inputValue;
+	selectStartStation.removeAttribute('hidden');
+};
 
-// function showHint(str) {
-// 	var hint = '';
-// 	if (str.length > 0) {
-// 		for (var i = 0; i < stations.length; i++) {
-// 			if (stations[i].name.toLowerCase().startsWith(str.toLowerCase())) {
-// 				hint +=
-// 					'<option value="' +
-// 					stations[i].name +
-// 					'">' +
-// 					stations[i].name +
-// 					'</option>';
-// 			}
-// 		}
-// 	}
-// 	var hintList = document.getElementById('txtHint');
-// 	if (hint.length > 0) {
-// 		hintList.innerHTML = hint;
-// 		hintList.style.display = 'block';
-// 	} else {
-// 		hintList.innerHTML = '';
-// 		hintList.style.display = 'none';
-// 	}
-// };
+// choosing of arrival station
+function selectListArrivalStation() {
+	const selectedValue = selectArivalStation.value;
+	console.log("selectedValue", selectedValue);
+	inputArrivalStation.value = selectedValue;
+	//  close list
+	selectArivalStation.setAttribute('hidden', 'true');
+};
 
+function changeValueArrivalInput() {
+	const inputValue = inputArrivalStation.value;
+	selectArivalStation.value = inputValue;
+	selectArivalStation.removeAttribute('hidden');
+};
